5ba7d4771ab10812ce77103674afbd2aaa5efba4
Ondrej Sika <ondrej@ondrejsika.com>
2022-03-26 20:22:47 +0100

    feat(github-actions-runner): Check required env vars

--- github-actions-runner/script.sh
+++ github-actions-runner/script.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+set -e
+
+slu shell-scripts required-environment-variables -e GITHUB_REPOSITORY_URL -e GITHUB_TOKEN
+
 ./config.sh --url $GITHUB_REPOSITORY_URL --token $GITHUB_TOKEN --unattended
 ./run.sh
